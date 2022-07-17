b0b2166272fd5cfc9e52ea6d98b0c98615911320
Samuele Favazza <sfavazza.github@gmail.com>
2022-07-17 12:10:43 +0000

    Ad GCM and initialization script

---
+++ dockerfiles/emacs/support/initscript.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# update certificates (needed for company)
+update-ca-certificates
+
+# initialize the GPG agent for GCM
+gpg --import ~/store/*.gcm
+
+# Use grep and awk to extract the key ID (most probably it can be achieved in an easier way, but for now should
+# suffice).
+pass init $(gpg --list-secret-keys --keyid-format LONG | grep --only-matching -E "(sec[[:space:]]*[a-z0-9]*/)([A-F0-9]+)" | awk 'split($0, A, "/") { print A[2] }')
