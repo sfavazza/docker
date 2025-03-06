fab521a4761ba21b0dcc2495aaff5b8f1d2cf49a
Samuele Favazza <sfavazza.github@gmail.com>
2025-03-06 22:00:09 +0100

    chores: remove old fontset archive & improve convenience script

--- init-build.sh
+++ init-build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# Utility to populate the script folder of a docker-file context with some common (versioned)
+# shell-script (in "common_scripts/") useful in the image being built by the target docker context.
 
 # list of environments requiring the common_scripts
-REQUIRE_COMMON_SCRIPTS=(emacs statistics)
+REQUIRE_COMMON_SCRIPTS_SUB_FOLDER=(
+    emacs
+    statistics
+)
 
 # copy from general folder
-for context in ${REQUIRE_COMMON_SCRIPTS[@]}; do
+for context in ${REQUIRE_COMMON_SCRIPTS_SUB_FOLDER[@]}; do
     _DST=dockerfiles/$context/scripts
     mkdir -p $_DST
     cp -u -v common_scripts/* $_DST
