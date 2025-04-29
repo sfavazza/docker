ba58f18ee3cfec1b30b0a4459220db23e855c394
Samuele Favazza <samuele.favazza@duagon.com>
2025-04-29 20:34:53 +0200

    feat(spacemacs): add rsync, improve build script

--- dockerfiles/spacemacs/build-spacemacs-image.sh
+++ dockerfiles/spacemacs/build-spacemacs-image.sh
@@ -2,12 +2,26 @@
 
 set -euxo pipefail
 
-# This script can be executed from a docker container, where the "dockerhostgroup" exists if the
-# "docker" group ID differs from the host one. If executed from the host the "dockerhostgroup" won't
-# exist and the original "docker" GID will be taken.
-docker_gid="$( (getent group dockerhostgroup || getent group docker) | awk -F ':' '{ print $3 }' )"
+# This script can be executed from a docker container, where either the "dockerhostgroup" or the a
+# group that happens to feature the same another group with the same host group ID. Consider adding
+# the user to whatever group is identified owning the host-docker-socket.
+# FIXME: this script shall be executed only from the host as ID for the docker group on the host
+# could correspond, in the docker, to:
+# - no group at all
+# - to another group which happen to have the same ID of the docker group on the host
+#
+# In the second scenario the dockerhostgroup won't exist as an already valid GID is present in the
+# image, but the 'docker' group in the image is not guaranteed to feature the same GID as the host
+# GID.
+#
+# NOTE: the file is considered from a running Emacs instance mounting the host /var/run/docker.sock
+# into the container this script is executed from.
+docker_gid="$(stat -c '%g' /var/run/docker.sock)"
+# TEMP: keep in case discover an untested case, the one where user id corresponds to an user
+# unrecognized by the Emacs container.
+# docker_gid="$( (getent group dockerhostgroup || getent group docker) | awk -F ':' '{ print $3 }' )"
 
-read -p 'spacemacs container tag: ' container_tag
+read -rp 'spacemacs container tag: ' container_tag
 
 docker build \
        --build-arg HOST_DOCKER_GID="$docker_gid" \
